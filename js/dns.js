478f9679e1738d14481ec4ceeb71010db5ae0248
Jim Canright <jim@canright.net>
2016-01-09 07:25:11 -0800

    interim stuff - get git together

--- js/dns.js
+++ js/dns.js
@@ -1,67 +1,140 @@
-"use strict";
+'use strict';
 
-var dns = require('dns');
+/* wraps and consolides the node dns module with a promise for a consolidated lookup similar to this:
 
-var resolveRr = (hostname, resolver) => {
-  return new Promise ((resolve, reject) => {
-    resolver(hostname, (err, hits) => {resolve(err?null:hits)});
-  });
-};
+  exports.lookup = (host, subs) => {
+    dns.getServers();
+    dns.resolve(host, eachof(["NS", "SOA", "A", "AAAA", "CNAME", MX", "TXT", "SRV", "PTR"])
+    dns.lookup(base host and each of host's subdomains in subs)
+  }
+  .then(report);  // records collected from all above
 
-exports.lookup = (rqsthost, subdoms) => {
+  where the host is the host domain name that has a nameserver
+  and subs is a list of subdomain prefexis to check for A records in addition to the base domain.
+  if subs is empty, the default lookup list, ["", www.", "api.", "rest.", "mail.", "ftp."]) is used.
+  promise resolution records each has the distinctive properties of the associated rrtype.
+  Reverse lookups (associated hostnames) are performed for each IP address lookup resuld  referenced in A, AAAA, CNAME, or MX records.
+
+  report: {
+    rqsthost: the hostname to resolve, like 'canright.com'.
+    subdomains: the subdomains to lookup, like ["www", "ftp"].
+    servers: array returned from dns.getServers(),
+    lookups: array of the results of lookups for the host and subdomains
+    NS: array of NS records resolved
+    SOA: SAO record resolved.
+    A: array of A records resolved.
+    AAAA: array of AAAA records resolved.
+    CNAME: array of CNAME records resolved.
+    MX:  array of MX records resolved.
+    TXT: array of TXT records resolved.
+    SRV: array of SRV records resolved.
+    PTR: array of PTR records resolved.
+    REVERSE: array of ip addresses referenced each with an array of lookups{ip, reverses}
+  }
+
+  exports.lookupService(address, port) then(hostname)
+*/
+
+const dns = require('dns'),
+  stds    = ['www', 'api', 'rest', 'mail', 'ftp'],
+  rrtypes = ['NS', 'SOA', 'A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SRV']; // 'PTR'
+exports.lookup = (rqsthost, rqstsubs) => {
+  var subs = rqstsubs.length ? rqstsubs : stds;
   return new Promise ((resolve, reject) => {
     if (!rqsthost)
-      reject("Domain name required");
+      reject('Domain name required');
     else {
       var rpt = {
-        rqsthost: rqsthost,
-        subdomains: subdoms?subdoms:[],
-        servers: dns.getServers(),
-        lookups: []
-      };
-
-      var look = (dom) => {
-        return new Promise ((resolve, reject) => {
+          rqsthost:   rqsthost,
+          subdomains: subs,
+          servers:    dns.getServers(),
+          lookups:    [],
+          reverses:   {}
+        },
+        ips  = {},
+        loks = [],
+        revs = [],
+        lkup = dom => new Promise ((resolve, reject) => {
           dns.lookup(dom, (err, address, family) => {
             if (!err) {
-              dns.reverse(address, (err, hostnames) => {
-                rpt.lookups.push({dom: dom, address: address, family: family, hostnames: err?null:hostnames});
-                resolve();
-              });
-            } else
-              reject(err);
+              rpt.lookups.push({dom: dom, address: address, family: family});
+              ips[address] = [];
+              resolve();
+            } else {
+              rpt.lookups.push({dom: dom, address: 'none'});
+              resolve();              
+            }
           });
-        });
-      };
+        }),
+        rvrs = ip => new Promise ((resolve, reject) => {
+          console.log('rvrs(%s)', ip);
+          dns.reverse(address, (err, revhosts) => {
+            if (!err) {
+              ips[address] = revhosts;
+              resolve();
+            } else
+              resolve();
+          });
+        }),
+        rslv = (hostname, rrtype) => {
 
-      var chks = [];
-      chks.push(look(rqsthost));
-      for (let k=0, knt=subdoms.length; k<knt; ++k) {
-        chks.push(look([subdoms[k], rqsthost].join(".")));
+          var prom = new Promise ((resolve, reject) => {
+            dns.resolve(hostname, rrtype, (err, hits) => {resolve(err?null:hits)});
+          });
+          loks.push(prom);
+          prom.then (recs => {
+            rpt[rrtype] = recs;
+            if (rrtype === 'A' || rrtype === 'AAAA')
+              if (recs)
+                for(let k=0, knt=recs.length; k<knt; ++k) {
+                  ips[recs[k]] = [];
+                }
+          })
+          .catch(err  => {
+            console.log('rslv(%s, %s) err: %s.', hostname, rrtype, err);
+            console.log(`rslv(${hostname}, ${rrtype}) err: ${err}`)
+          });
+        };
+      console.log('lookup root: %s.', rqsthost);
+      loks.push(lkup(rqsthost));
+      for (let k=0, knt=subs.length; k<knt; ++k) {
+        loks.push(lkup([subs[k], rqsthost].join('.')));
       }
-      let pc = resolveRr(rqsthost, dns.resolveCname); chks.push(pc); pc.then((cnms) => {rpt.cnames = cnms});
-      let pm = resolveRr(rqsthost, dns.resolveMx);    chks.push(pm); pm.then((mxs ) => {rpt.mxs    = mxs });
-      let pn = resolveRr(rqsthost, dns.resolveNs);    chks.push(pn); pn.then((nss ) => {rpt.nss    = nss });
-      let ps = resolveRr(rqsthost, dns.resolveSoa);   chks.push(ps); ps.then((soa ) => {rpt.soa    = soa });
-      let pv = resolveRr(rqsthost, dns.resolveSrv);   chks.push(pv); pv.then((srvs) => {rpt.srvs   = srvs});
-      let pt = resolveRr(rqsthost, dns.resolveTxt);   chks.push(pt); pt.then((txts) => {rpt.txts   = txts});
-      Promise.all(chks)
-      .then (()    => {
-        resolve(rpt);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-    }
-  //    dns.lookupService(address, 3000, (err, hostname, service) => {
-  //      if (!err) {
-  //        rpt.hostname = hostname;
-  //        rpt.service = service;
-  //        rpt.msgs.push("lookupService ok");
-  //      } else
-  //        rpt.msgs.push("lookupService error: " + err);
-  //    });
 
-  //  console.log("looking...");
+      for(let k = 0; k < rrtypes.length; ++k)
+        rslv(rqsthost, rrtypes[k]);
+      Promise.all(loks)
+      .then( () => {
+        console.log('lookups done with %d ips.', Object.keys(ips).length);
+        if (Object.keys(ips).length) {
+          for (let ip in ips) {
+            revs.push(rvrs(ip));
+          }
+          for (let k=0, knt=revs.length; k<knt; ++k)
+            console.log("REVERSES %s", JSON.stringify(revs[k]));
+        } else {
+          console.log('nothing in ips');
+
+        }
+        console.log("ips itterated: %d", revs.length);
+        if (revs.length) {
+          console.log("got revs (%d)", revs.length);
+          Promise.all(revs)
+          .then( () => {
+            ()  => {
+              console.log('with reverses...');
+              resolve(rpt);
+            },
+            err => {
+              console.log('reversing error: %s', err);
+              resolve(rpt);
+            }
+          });
+        } else {
+          console.log("no reverses");
+          resolve(rpt);
+        }
+      });
+      }
   });
 };
--- lookup.js
+++ lookup.js
@@ -1,50 +1,107 @@
-"use strict";
+'use strict';
 
-var dns = require('./js/dns.js');
+const xp = require('express'),
+  morgan = require('morgan'),
+  parser = require('body-parser'),
+  stdin  = process.openStdin(),
+  app    = xp();
 
-var express  = require("express"),
-  morgan     = require("morgan"),
-  bodyParser = require("body-parser"),
-  app        = express();
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan("dev"));
+console.log('- Serving: localhost:%s\n', process.env.PORT || 3000);
 
-console.log("- Serving: localhost:%s\n", process.env.PORT || 3000);
+app.get('/dns.html', (req, res) => {res.sendFile(req, res, '/dns.html')});
 
-app.get("/dns/*", (req, res) => {
-  var prts = req.path.slice(5).split(",");
-//var rqst = req.path.slice(5);
-//var prts = rqst.split(",");
+const render = (isH, rpt) => {
+  const nn  = n     => n + isH ? '' : ' '.repeat( 8-n.length);
+  const pp  = p     => p + isH ? '' : ' '.repeat(10-p.length);
+  const ln  = s     => isH ? '<li>' + s + '</li>' : s; // string
+  const nv  = (n,v) => ln(nn(n) + v); // name valuey, value)
+  const opv = (n,o) => { // object property values(name, object)
+    var r = '';
+    for (var p in o) {
+      if (o.hasOwnProperty(p))
+        r += ln(nn(n) + pp(p) + ': ' + o[p]) + '\n  ';
+    }
+    return r;
+  }
+  const aiv = (n, a)  => a ? a.map(v => ln(nn(n) + v)).join('\n  ')
+                           : ln(nn(n) + 'none');
+  const aio = (n, a)  => a ? a.map(v => ln(nn(n) + JSON.stringify(v))).join('\n  ')
+                           : ln(nn(n) + 'none');
+  return `
+  ${isH?'<ul>':'-- DNS LOOKUP -----'}
+  ${nv ('HOST',   rpt.rqsthost)}
+  ${nv ('SUBS',   rpt.subdomains)}
+  ${opv('SOA',    rpt.SOA)}
+  ${aiv('SERVER', rpt.servers)}
+  ${aio('NS',     rpt.NS)}
+  ${aio('LOOK',   rpt.lookups)}
+  ${aio('A',      rpt.A)}
+  ${aiv('AAAA',   rpt.AAAA)}
+  ${aio('CNAME',  rpt.CNAME)}
+  ${aio('MX',     rpt.MX)}
+  ${aio('SRV',    rpt.SRV)}
+  ${aio('TXT',    rpt.TXT)}
+  ${isH?'</ul>':'-------------------'}
+  ${isH?'<hr>':''}
+  ${isH?'<!--':''}
+${isH?JSON.stringify(rpt):''}
+  ${isH?'-->':''}
+`;
+}
+
+const dns = require('./js/dns.js');
+
+/*
+app.get("/dns/:host/:subs", (req, res) => {
+*/
+app.get('/dns/*', (req, res) => {
+
+  var prts = req.path.slice(5).split(',');
 
   if (!prts.length)
-    res.status(404).send("an interesting query...");
+    res.status(404).send('an interesting query...<br>try /dns/host/subs');
   else {
     var host = prts[0];
     var subs = (prts.length>1)? prts.slice(1):[];
-    var msgQ = [];
-
-    var say = (key, val) => {
-      msgQ.push(key + ": " + host);
-      console.log("%s: %s", key, val);
-    }
-
-    res.status(404);
-
-    say("Host", host);
-    if (prts.length > 1)
-      say("subs", subs.join(", "));
 
     dns.lookup(host, subs)
-    .then ((rpt) => {
-      res.status(200).send(JSON.stringify(rpt));
-    })
-    .catch((err) => {
-      res.status(403).send("whoops: " + err);
-    });
+    .then (rpt => {res.status(200).send(render(1,rpt))})
+    .catch(err => {res.status(500).send(`dns.lookup error: ${err}`)});
   }
 });
 
 app.listen(process.env.PORT || 3000);
+
+stdin.addListener('data', function(d) {
+  var o = JSON.parse(JSON.stringify(d));
+  var lin = d.toString().trim();
+  var rgs = lin.split(" ");
+  var len = rgs.length;
+  if (!len)
+    console.log('Usage: dns');
+  else {
+    switch (rgs[0]) {
+      case 'dns':
+          if (len<2)
+            console.log('Usage: dns hostname [subdomains]');
+          else {
+            let subs = [];
+            if (len>2)
+              for (var i=2;i<len;++i)
+                subs.push(rgs[i]);
+            dns.lookup(rgs[1], subs)
+              .then (rpt => {console.log(render(0,rpt))})
+              .catch(err => {console.log('dns.lookup error: %s', err)});
+          }
+          break;
+        default:
+          console.log('unrecognized command');
+      }
+    }
+  });
+
 module.exports = app;
