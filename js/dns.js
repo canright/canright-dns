5b4913e270dc0e31c84df1b86336e01f9adc3347
Jim Canright <jim@canright.net>
2016-03-18 23:07:35 -0700

    typos

--- js/dns.js
+++ js/dns.js
@@ -175,7 +175,7 @@ const dns  = require('dns'),
                 logger(ident, 'reverse lookup error: ' + JSON.stringify(err));
                 rpt.ips[address] = [];
                 resolve([]);
-            });
+            });
           }),
 
           hostResolve = (host, rrtype) => {
